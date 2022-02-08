c8d01af8043b2a714b479d5e1ed671286b671b0d
horidhenry <robarnold100@hotmail.com>
2022-02-08 10:17:18 +0000

    right stuff returned

--- printify/views.py
+++ printify/views.py
@@ -12,7 +12,7 @@ class CreateProductValidation(forms.Form):
     chartBase64 = forms.CharField()
     colour = forms.CharField()
     market = forms.CharField()
-    # sizes = forms.JSONField()
+    sizes = forms.JSONField()
     symbol = forms.CharField()
 
 @csrf_exempt
@@ -44,87 +44,93 @@ def create_product(request):
             }
 
             imageResponse = requests.request("POST", imagesUrl, headers=headers, data=imagePayload)
-            imageId = json.loads(imageResponse.text)['id']
+            if imageResponse.status_code == 200:
+                imageId = json.loads(imageResponse.text)['id']
 
-            productUrl = "https://api.printify.com/v1/shops/" + os.environ.get('PRINTIFY_STORE_ID') + "/products.json"
+                productUrl = "https://api.printify.com/v1/shops/" + os.environ.get('PRINTIFY_STORE_ID') + "/products.json"
 
-            price = int(os.environ.get('TEE_PRICE'))
+                price = int(os.environ.get('TEE_PRICE'))
 
-            productPayload = json.dumps({
-                "title": title,
-                "description": "Stock Tee",
-                "blueprint_id": 6,
-                "print_provider_id": 61,
-                "variants": [
-                    {
-                        "id": 12102,
-                        "sku": "85393203832296141203",
-                        "price": price,
-                        "is_enabled": True
-                    },
-                    {
-                        "id": 12101,
-                        "sku": "30967999484636289818",
-                        "price": price,
-                        "is_enabled": True
-                    },
-                    {
-                        "id": 12100,
-                        "sku": "26113178150344651753",
-                        "price": price,
-                        "is_enabled": True
-                    }
-                ],
-                "print_areas": [
-                    {
-                        "variant_ids": [
-                            12102,
-                            12101,
-                            12100
-                        ],
-                        "placeholders": [
+                productPayload = json.dumps({
+                    "title": title,
+                    "description": "Stock Tee",
+                    "blueprint_id": 6,
+                        "print_provider_id": 61,
+                        "variants": [
                             {
-                                "position": "front",
-                                "images": [
+                                "id": 12102,
+                                "sku": "85393203832296141203",
+                                "price": price,
+                                "is_enabled": True
+                            },
+                            {
+                                "id": 12101,
+                                "sku": "30967999484636289818",
+                                "price": price,
+                                "is_enabled": True
+                            },
+                            {
+                                "id": 12100,
+                                "sku": "26113178150344651753",
+                                "price": price,
+                                "is_enabled": True
+                            }
+                        ],
+                    "print_areas": [
+                            {
+                                "variant_ids": [
+                                    12102,
+                                    12101,
+                                    12100
+                                ],
+                                "placeholders": [
                                     {
-                                        "id": imageId,
-                                        "name": "",
-                                        "x": 0.5,
-                                        "y": 0.3,
-                                        "scale": 1,
-                                        "angle": 0
+                                        "position": "front",
+                                        "images": [
+                                            {
+                                                "id": imageId,
+                                                "name": "",
+                                                "x": 0.5,
+                                                "y": 0.3,
+                                                "scale": 1,
+                                                "angle": 0
+                                            }
+                                        ]
                                     }
                                 ]
                             }
-                        ]
-                    }
-                ],
-                "print_details": []
-            })
+                        ],
+                    "print_details": []
+                    })
 
-            productResponse = requests.request("POST", productUrl, headers=headers, data=productPayload)
+                productResponse = requests.request("POST", productUrl, headers=headers, data=productPayload)
 
-            productResFormatted = json.loads(productResponse.text)
+                productResFormatted = json.loads(productResponse.text)
 
-            if 'id' in productResFormatted:
+                if 'id' in productResFormatted:
 
-                publishPayload = json.dumps({
-                    "title": True,
-                    "description": True,
-                    "images": True,
-                    "variants": True,
-                    "tags": True,
-                })
+                    publishPayload = json.dumps({
+                        "title": True,
+                        "description": True,
+                        "images": True,
+                        "variants": True,
+                        "tags": True,
+                    })
 
-                publishUrl = "https://api.printify.com/v1/shops/" + os.environ.get('PRINTIFY_STORE_ID') + "/products/" + productResFormatted['id'] + "/publish.json"
-                publishResponse = requests.request("POST", publishUrl, headers=headers, data=publishPayload)
+                    publishUrl = "https://api.printify.com/v1/shops/" + os.environ.get('PRINTIFY_STORE_ID') + "/products/" + productResFormatted['id'] + "/publish.json"
+                    publishResponse = requests.request(
+                        "POST", publishUrl, headers=headers, data=publishPayload)
 
+                    if publishResponse.status_code == 200:
+                        productResFormatted['sizes'] = body['sizes']
 
-                #include requested sizes / variant ids in the response
-
-                return HttpResponse(publishResponse.text, status=publishResponse.status_code)
+                        return HttpResponse(json.dumps(productResFormatted), status=publishResponse.status_code)
+                    else:
+                        return HttpResponse(publishResponse.text, status=publishResponse.status_code)
+                else:
+                    return HttpResponse(productResponse.text, status=productResponse.status_code)
             else:
-                return HttpResponse(productResponse.text, status=productResponse.status_code)
+                return HttpResponse(imageResponse, status=imageResponse.status_code)
         else:
             return HttpResponse('Unprocessable Entity', status=422)        
     else:
